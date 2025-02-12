92812732611f07a0231991e6e0c309d0394f3a5f
adomas.wav <adlukosev2@gmail.com>
2025-02-12 16:22:46 +0200

    Refactor student data structure and update input/output handling

--- OOP1.cpp
+++ OOP1.cpp
@@ -1,25 +1,39 @@
 #include "paz_lib.h"
 
-int main() 
+int main()
 {
-cout << "Iveskite namu darbu (nd) skaiciu: ";
-cin >> n;
+    cout << "Iveskite namu darbu pazymiu skaiciu: ";
+    cin >> n;
 
-vector<stud> grupe;
-for(int i = 0; i < k; i++)
+    vector<stud_struct> grupe;
+
+    // Ivedimas
+    for (int i = 0; i < k; i++)
     {
-    stud laik;
-    cout << "Iveskite mokinio varda: ";
-    cin >> laik.var;
-    cout << "Iveskite mokinio pavarde: ";
-    cin >> laik.pav;
-    for(auto& nd : laik.paz)
+        stud_struct temp;
+        cout << "Iveskite mokinio varda: ";
+        cin >> temp.var;
+        cout << "Iveskite mokinio pavarde: ";
+        cin >> temp.pav;
+        for (int i = 0; i < n; i++)
         {
-        cout << "Iveskite mokinio pazymi: ";
-        cin >> nd;
+            int pazymys;
+            cout << "Iveskite mokinio pazymi " << i + 1 << ": ";
+            cin >> pazymys;
+            temp.paz.push_back(pazymys);
         }
+        cout << "Iveskite egzamino rezultata: ";
+        cin >> temp.egz;
+        grupe.push_back(temp);
+    }
+
     
-    grupe.push_back(laik);
+
+    cout << setw(15) << left << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << endl;
+    cout << "-------------------------------------------------------" << endl;
+    for (stud_struct &nd : grupe)
+    {
+        cout << setw(15) << left << nd.var << setw(15) << nd.pav << setw(20) << endl;
     }
 
     return 0;
--- paz_lib.h
+++ paz_lib.h
@@ -1,15 +1,20 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
 using std::vector;
 using std::string;
 using std::cin;
 using std::cout;
+using std::endl;
+using std::left;
+using std::setw;
 
 int k = 1; // mokinių skaičius
 int n = 5; // pazymiu skaičius
 
-struct stud{
+struct stud_struct{
     string var, pav;
     vector<int> paz;
     int egz;
+    double galutinis;
 };
